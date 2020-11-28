f23e0c10a6113c8690e28373e8ef5f292f3e5658
Степан <stepantishhen@mail.ru>
2020-11-28 08:22:27 +0300

    program

--- main.py
+++ main.py
@@ -1,6 +1,68 @@
-def write():
-    print('My first git-repo')
+import sys
+from PyQt5.QtWidgets import QWidget, QApplication
+from PyQt5.QtCore import Qt
+from PyQt5.QtGui import QPainter
+
+
+class Example(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+        self.setMouseTracking(True)
+        self.coords_ = []
+        self.qp = QPainter()
+        self.flag = False
+        self.status = None
+
+    def drawf(self):
+        self.flag = True
+        self.update()
+
+    def paintEvent(self, event):
+        if self.flag:
+            self.qp = QPainter()
+            self.qp.begin(self)
+            self.draw(self.status)
+            self.qp.end()
+
+    def draw(self, status):
+        if status == 1:
+            self.qp.drawEllipse(*self.coords_, 20, 20)
+        elif status == 2:
+            self.qp.drawRect(*self.coords_, 20, 20)
+        elif status == 3:
+            x, y = self.coords_
+            R = 15
+            a = 13
+            coords = [(x, y + R), (x + a, y - R), (x - a, y - R)]
+            self.qp.drawLine(*coords[0], *coords[1])
+            self.qp.drawLine(*coords[1], *coords[2])
+            self.qp.drawLine(*coords[2], *coords[0])
+
+    def initUI(self):
+        self.setGeometry(300, 300, 300, 300)
+        self.setWindowTitle('Рисование')
+
+        self.show()
+
+    def mousePressEvent(self, event):
+        self.coords_ = [event.x(), event.y()]
+        if (event.button() == Qt.LeftButton):
+            self.status = 1
+        elif (event.button() == Qt.RightButton):
+            self.status = 2
+        self.drawf()
+
+    def mouseMoveEvent(self, event):
+        self.coords_ = [event.x(), event.y()]
+
+    def keyPressEvent(self, event):
+        if event.key() == Qt.Key_Space:
+            self.status = 3
+        self.drawf()
 
 
 if __name__ == '__main__':
-    write()
+    app = QApplication(sys.argv)
+    ex = Example()
+    sys.exit(app.exec_())
